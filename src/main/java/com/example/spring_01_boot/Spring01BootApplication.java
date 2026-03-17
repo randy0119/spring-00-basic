3b09a6e33ab84dca5bed2e793f9dab6ce412bd78
LeeDeokgyu(Randy) <doek0119@gmail.com>
2026-03-17 23:15:37 +0900

    Initial commit

---
+++ src/main/java/com/example/spring_01_boot/Spring01BootApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_01_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring01BootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring01BootApplication.class, args);
+	}
+
+}
