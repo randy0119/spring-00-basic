7756461bfbea5959bbf4cb601090fa3a31fa55c5
LeeDeokgyu(Randy) <doek0119@gmail.com>
2026-03-18 14:14:20 +0900

    [ADD] 레포지토리 테스트 보강

--- src/test/java/com/example/spring_01_boot/user/repository/MemberRepositoryTest.java
+++ src/test/java/com/example/spring_01_boot/user/repository/MemberRepositoryTest.java
@@ -3,6 +3,9 @@ package com.example.spring_01_boot.user.repository;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.example.spring_01_boot.user.repository.entity.Member;
+
+import java.util.NoSuchElementException;
+
 import org.junit.jupiter.api.Test;
 
 @SpringBootTest
@@ -13,10 +16,10 @@ public class MemberRepositoryTest {
     @Test
     public void crudTest() {
         Member member = Member.builder()
-            .id("test")
-            .name("test")
-            .email("test@test.com")
-            .password("test")
+            .id("test-id")
+            .name("test-name")
+            .email("test-email@test.com")
+            .password("test-password")
             .build();
 
         // create test
@@ -24,5 +27,18 @@ public class MemberRepositoryTest {
 
         // get test
         Member foundMember = memberRepository.findById(1L).get();
+        System.out.println(foundMember);
+
+        // delete test
+        memberRepository.delete(member);
+
+        // get test
+        try {
+            Member foundMember2 = memberRepository.findById(1L).get();
+            System.out.println(foundMember2);
+        } catch (NoSuchElementException e) {
+            System.out.println("Member not found: " + e.getMessage());
+            System.out.println("delete test success!");
+        }
     }
 }
