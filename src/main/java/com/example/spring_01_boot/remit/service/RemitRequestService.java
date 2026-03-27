0e55da62948ee7c3ff17d6bfd7094759a417dca9
LeeDeokgyu(Randy) <doek0119@gmail.com>
2026-03-27 19:51:42 +0900

    FIX(Remit): 송금 요청 생성 서비스 인터페이스 파라미터 오탈자 수정

--- src/main/java/com/example/spring_01_boot/remit/service/RemitRequestService.java
+++ src/main/java/com/example/spring_01_boot/remit/service/RemitRequestService.java
@@ -1,8 +1,7 @@
 package com.example.spring_01_boot.remit.service;
 
 import com.example.spring_01_boot.remit.dto.RemitRequestResponse;
-import com.example.spring_01_boot.remit.repository.entity.RemitRequestStatus;
 
 public interface RemitRequestService {
-    RemitRequestResponse createRemitRequest(String requestId, String receiverId, Integer amount);
+    RemitRequestResponse createRemitRequest(String requesterId, String receiverId, Integer amount);
 }
